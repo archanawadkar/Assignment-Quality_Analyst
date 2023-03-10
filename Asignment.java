bb032264cacfbcf947bb6331e6f2cc8fa0448faa
archanawadkar <107843887+archanawadkar@users.noreply.github.com>
2023-03-10 11:48:54 +0530

    Add files via upload
    Assignment completed

---
+++ Asignment.java
@@ -0,0 +1,59 @@
+package Test;
+
+import static org.testng.Assert.assertTrue;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Asignment {
+    public static void main(String[] args) {
+		
+       System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver_win32 (3)\\chromedriver.exe");
+	  
+       ChromeOptions options=new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+       
+       WebDriver driver=new ChromeDriver(options);
+	   driver.get("http://uitestingplayground.com/");
+	   driver.manage().window().maximize();
+	 	
+       driver.findElement(By.xpath("//a[contains(text(),'Click')]")).click();
+    	
+       driver.navigate().back();
+       
+       driver.findElement(By.xpath("//a[contains(text(),'Text Input')]")).click();
+    	
+	   driver.findElement(By.xpath("//input[@id=\"newButtonName\"]")).sendKeys("welcome to assignment");
+	   
+	   WebElement button=driver.findElement(By.xpath("//button[@id='updatingButton']"));
+	   
+	   Actions act=new Actions(driver);
+	   act.moveToElement(button).click().build().perform();
+	  
+	   String text=button.getText();
+	   if(text.equalsIgnoreCase("welcome to assignment")) {
+		   System.out.println("Element is clickble");
+	   }
+	   else {
+		   System.out.println("Element is not clickable");
+	   }
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	}
+}
